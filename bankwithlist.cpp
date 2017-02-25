42584144b12e3e8cc249aa4f2305970bfacfd69f
Matthew K Yamamoto <mky99@wiliki.eng.hawaii.edu>
2017-02-24 19:41:33 -1000

    Updated driver

--- bankwithlist.cpp
+++ bankwithlist.cpp
@@ -1,22 +1,40 @@
-/* EE 205 Homework Assignment #2
- *
- * Name   :
- * Date   :
- * Team   :
- * Members:
- */
+//driver.cpp
+//Matthew Yamamoto
+//02/24/17
 
-#include <iostream>
-#include "singlylinked.h"
+//#include "singlylinked.h"
+#include<iostream>
 using std::cin;
 using std::cout;
 using std::endl;
 
+void sort(SLinkedList list,BasicAccount* ba){
+	Snode* current = list.getHead();
 
-int main(){
+	while(current){
+		if(ba->getid == current->getid){
+		list.insert(ba, current)
+		return 0;
+		}
+		current = current->next;
+	}
+	return 0;
+}
 
-    SLinkedLink<int> list;
-    cout << "Success!" << endl;
 
-    return 0;
+main(){
+
+	SLinkedList();
+	
+	for(int i = 0; i < 3; i++){
+		BasicAccount b1 = BasicAccount();
+		SLinkedList::copy();
+	}
+
+	SLinkedList::find();
+	SLinkedList::find();
+
+	SLinkedList::erase();
+	SLinkedList::erase();
+
 }
