4b0b2b56e3e7408a25181ca5ceff51f4526fb1f5
Paulo Lemus <paulolemus14@gmail.com>
2017-02-25 21:03:12 -1000

    Initialized bankwithlist.cpp

--- bankwithlist.cpp
+++ bankwithlist.cpp
@@ -3,33 +3,21 @@
 //02/24/17
 //This program will be the driver that tests our bank account list
 
-//#include "singlylinked.h"
+#include "BasicAccount.h"
+#include "singlylinked.h"
 #include<iostream>
+using std::cout;
+using std::cin;
 using std::endl;
 
-void sort(SLinkedList list,BasicAccount* ba){	//function sort to add new basicaccounts
-	Snode* current = list.getHead();
-
-	while(current){
-		if(ba->getid > current->data->getid()){	//if basicaccount id is greater than current id
-		list.insert(ba, current)	//insert basic account and end
-		return 0;
-		}
-		if(ba->getid == current->data->getid()){	//if id is equal to, account number
-			std::cout << "Account number invalid" << endl;	//invalid, end
-			return 0;
-		}
-		current = current->next;	//iterate
-	}
-	return 0;
-}
 
 
-main(){
+int main(){
 
-	SLinkedList();
+    SLinkedList<BasicAccount> list;
 
-	while(){
-	
-	}
+    
+
+    cout << "\n\nGoodbye!\n\n";
+    return 0;
 }
