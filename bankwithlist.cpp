f996763cff7c38add2a2c01407fedc993a9712e6
Matthew K Yamamoto <mky99@wiliki.eng.hawaii.edu>
2017-02-25 21:35:20 -1000

    driver just looks for flag, didnt input functions

--- bankwithlist.cpp
+++ bankwithlist.cpp
@@ -53,25 +53,37 @@ int main(){
 	// SLinkedList();
 	float a =0; 
 	SLinkedList<BasicAccount> list; //creates a list of type BasicAccount
+	char flag = 'a';
+
+while(flag !='q')	{
+
+	cout << "a = add, f = find, d = delete, q = quit" << endl;
+	cin >> flag;
+
+	if(flag == 'a'){}
+	if(flag == 'f'){}
+	if(flag == 'd'){}
+	if(flag == 'q'){}
 	
-    BasicAccount ba0; //constructs a basic acct class ba0
-    ba0.setAccountNum(500);
-    ba0.setName("Alice");
-    ba0.setBalance(400);
+
+ //   BasicAccount ba0; //constructs a basic acct class ba0
+   // ba0.setAccountNum(500);
+   // ba0.setName("Alice");
+   // ba0.setBalance(400);
     
-    BasicAccount ba1; 
-    ba1.setAccountNum(10000);
-    ba1.setName("Tom");
-    ba1.setBalance(1234);
+  //  BasicAccount ba1; 
+//    ba1.setAccountNum(10000);
+  //  ba1.setName("Tom");
+//    ba1.setBalance(1234);
     
     
     
-    list.append(ba0);
-    list.append(ba1);
-	list.printAll();
+    //list.append(ba0);
+  //  list.append(ba1);
+//	list.printAll();
 	
-	cout << "What is your account number (see hints above^^): "; 
-	cin>> a; 
+//	cout << "What is your account number (see hints above^^): "; 
+//	cin>> a; 
 // 	cout << "debug: input was: "<<a << endl; 
 	
 	
@@ -79,9 +91,9 @@ int main(){
 // 	float aByAcct = list.findByAcct(ba1, ac)->data.getAccountNum(); 
 // 	cout << a << endl; 
 
-    SNode<BasicAccount>* ptr = list.findByAcct(ba1, a);
-    cout<< "Welcome "<< ptr->data.getName() << "!"<< endl; 
+   // SNode<BasicAccount>* ptr = list.findByAcct(ba1, a);
+   // cout<< "Welcome "<< ptr->data.getName() << "!"<< endl; 
     
-
+}
 	return 0;
 }
