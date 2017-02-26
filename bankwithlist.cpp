3294f5d37fe0351aaa2bfba483744e7d1a583a6b
Paulo Lemus <paulolemus14@gmail.com>
2017-02-25 21:35:54 -1000

    template bankwithlist main loop

--- bankwithlist.cpp
+++ bankwithlist.cpp
@@ -19,8 +19,35 @@ using std::cin;
 int main(){
 
 	SLinkedList<BasicAccount> list; //creates a list of type BasicAccount
-
+    
     // MAIN LOOP GOES HERE
+    while(true){
+        // Greet the teller, prompt what he would like to do
+        // "1 - Create account, 2 - Find a customer, 3 - exit
+        
+
+        // Create Account:
+        // enter Account ID:
+        // enter Account Name:
+        // Enter new Account balance:
+        // // create ba object, check if the same if exists in list,
+        //          if it exists, return error, continue to next loop
+        //          if it is valid, insert and let teller know success
+
+        // Find a customer:
+        // "would you like to seach by account id or name?
+        // 1 - ID, 2 - name
+        //
+        // ID: 
+        //      have user enter id. Give it and the list to jessies 
+        //      find by id function.
+        //       
+        //      next prompt user if he would like to withdraw, deposit
+        //      or return.
+        //
+        // name: Same thing as above but use jessies find by name function
+        //
+    }
 	
 
     cout << "\n\nGoodbye!\n\n";
