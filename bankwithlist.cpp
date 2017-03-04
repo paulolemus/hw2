7e498c2f9c0fdcfbe7a140674e4008b813bdc7de
paulolemus <paulolemus14@gmail.com>
2017-03-04 01:08:04 -1000

    Finished bankwithlist code and refactoring

--- bankwithlist.cpp
+++ bankwithlist.cpp
@@ -1,5 +1,5 @@
 /* File: bankwithlist.cpp
- * Name: Paulo Lemus
+ * Name: Paulo L, Jessie G, Matthew Y
  * Date: 02/24/17
  * Team: Sandwich
  */
@@ -13,45 +13,49 @@ using std::cin;
 using std::endl;
 using std::string;
 
-/* AddNewAccount:
- * This functions adds an account to the list
- * sorted by ID number. It searches for an id that
- * is greater than it, and inserts a new node before it.
- * If there are none or if list is empty, it appends the node.
- */
+/* This functions adds an account to the list sorted by ID. */
 template <typename T>
 SNode<T>* addNewAccount(SLinkedList<T>& list, BasicAccount& ba);
 
-
-/* SearchById && SearchByName:
- * These functions operate very similarly.
- * They first check if the head is null.
- * Otherwise, they iterate through each node comparing
- * the given string or id to the node name or id.
- * If there is a match, a ptr to the node is returned.
- * Otherwise, NULL is returned.
- */
+/* Return a pointer to a node with the matching id or name */
 template <typename T>
 SNode<T>* searchById(SLinkedList<T>& list, long int id);
 template <typename T>
 SNode<T>* searchByName(SLinkedList<T>& list, string name);
 
 
-/* depositOrWithdraw:
- * Prompts user to withdraw or deposit money into an
- * account given via pointer to node.
- */
+/* Add or remove money from a BasicAccount in the given node */
 template <typename T>
 void depositOrWithdraw(SNode<T>* ptr);
 
+/* Case functions */
+template <typename T>
+void caseAddCustomer(SLinkedList<T>& list);
+template <typename T>
+void caseDeleteCustomer(SLinkedList<T>& list);
+template <typename T>
+SNode<T>* caseFindCustomer(SLinkedList<T>& list);
 
 
 int main(){
-
+    
+    // Class to manage linkedList data structure
     SLinkedList<BasicAccount> list;
 
+    // Initial test cases
+    BasicAccount ba1(11123, "Bob", 1034.55);
+    BasicAccount ba2(11124, "Charlie", 500);
+    BasicAccount ba3(11122, "Alice", 20000);
+    addNewAccount(list, ba1);
+    addNewAccount(list, ba2);
+    addNewAccount(list, ba3);
+
+    /* Main while loop that handles user input and selects
+     * the appropriate function to call via a switch statement.
+     */
     while(true){
         int choice;
+        SNode<BasicAccount>* acctPtr = NULL;
 
         // Clean introduction
         cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
@@ -61,145 +65,166 @@ int main(){
         cout << "1 - Add new Account\n";
         cout << "2 - Find an existing customer\n";
         cout << "3 - Delete an existing customer\n";
-        cout << "4 - Burn everything and shutdown\n";
+        cout << "4 - Print all bank account information\n";
+        cout << "5 - Burn everything and shutdown\n";
         cin >> choice;
 
-        /* Break loop to exit code */
-        if(choice == 4) break;
-
-        /* Delete an existing customer:
-         * This block allows the user to first search
-         * through the list of accounts using name or ID,
-         * then displays the account information before
-         * requesting confirmation if this is the account
-         * to delete. If it is, it is deleted. Otherwise,
-         * no action is taken.
-         */
-        else if(choice == 3){
-            string confirm;
-            string nameOrId;
-            string searchStr;
-            SNode<BasicAccount>* ptr;
-
-            cout << "\n\nWould you like to delete by id or name? ";
-            cin >> nameOrId;
-
-            if(nameOrId == "id"){
-                cout << "\nEnter id: ";
-                cin >> searchStr;
-                ptr = searchById(list, std::stol(searchStr));
-            }
-            else if(nameOrId == "name"){
-                cout << "\nEnter name: ";
-                cin >> searchStr;
-                ptr = searchByName(list, searchStr);
-            }
-            else{
-                cout << "\nInvalid command\n";
-                cout << "Press enter to continue";
-                cin.ignore();
-                cin.get();
-                continue;
-            }
-            if(ptr == NULL){
-                cout << "\nCould not locate account.\n";
-                cout << "Press enter to continue";
-                cin.ignore();
-                cin.get();
-                continue;
-            }
-            cout << "Found account. Details below:\n\n";
-            cout << ptr->data;
-            cout << "\nAre you sure you want to delete the account? ";
-            cin >> confirm;
-            if(confirm[0] == 'y' || confirm[0] == 'Y'){
-                list.erase(ptr);
-                cout << "\nAccount deleted.\n\n";
-            }
-            else cout << "\nNo action taken.\n\n";
+        switch(choice){
+            case 1:
+                caseAddCustomer(list);
+                break;
+            case 2:
+                acctPtr = caseFindCustomer(list);
+                if(acctPtr != NULL) depositOrWithdraw(acctPtr);
+                break;
+            case 3:
+                caseDeleteCustomer(list);
+                break;
+            case 4:
+                cout << endl;
+                list.printAll();
+                break;
+            case 5:
+                cout << "\n\nGoodbye!\n\n";
+                return 0;
         }
-
-        /* Find an existing account by name or ID.
-         * This choice allows the user to select
-         * the desired method of searching, then prompts
-         * user to enter a name or id to searh by.
-         * If a valid account is found, it also prompts user
-         * if he/she would like to deposit or withdraw money.
-         */
-        else if(choice == 2) {
-            string nameOrId;
-            string searchStr;
-            SNode<BasicAccount>* ptr;
-
-            cout << "\n\nWould you like to search by id or name? ";
-            cin >> nameOrId;
-
-            if(nameOrId == "id"){
-                cout << "\nEnter id: ";
-                cin >> searchStr;
-                ptr = searchById(list, std::stol(searchStr));
-            }
-            else if(nameOrId == "name"){
-                cout << "\nEnter name: ";
-                cin >> searchStr;
-                ptr = searchByName(list, searchStr);
-            }
-            else{
-                cout << "\nInvalid command\n";
-                cout << "Press enter to continue";
-                cin.ignore();
-                cin.get();
-                continue;
-            }
-            if(ptr == NULL){
-                cout << "\nCould not locate account.\n";
-                cout << "Press enter to continue";
-                cin.ignore();
-                cin.get();
-                continue;
-            }
-            cout << "Found account. Details below:\n\n";
-            cout << ptr->data;
-            depositOrWithdraw(ptr);
-        }
-
-        /* Add a new account to the list. This first checks
-         * if the account is available by ID.
-         * If it is, it adds it, otherwise it indicates the
-         * account id is taken.
-         */
-        else if(choice == 1){
-            long int id;
-            string   name;
-            float    balance;
-            SNode<BasicAccount>* ptr;
-
-            cout << "\nTo create a new account, provide the following:\n";
-            cout << "Account id\t: ";
-            cin >> id;
-            cout << "Account name\t: ";
-            cin >> name;
-            cout << "Account balance\t: ";
-            cin >>balance;
-            BasicAccount ba(id, name, balance);
-            ptr = addNewAccount(list, ba);
-            if(ptr == NULL){
-                cout << "\nFailed to add account. ID already exists.\n";
-            }
-            else{
-                cout << "\nAccount successfully added.\n";
-                cout << ptr->data;
-            }
-        }
+        // Input error guards
+        if(cin.fail()) cout << "\nAn invalid command was entered\n";
         cout << "\nPress Enter to continue";
-        cin.ignore();
-        cin.get();
+        cin.clear();
+        cin.ignore(100, '\n');
+        while(cin.get() != '\n');
     }
-    cout << "\n\nGoodbye!\n\n";
     return 0;
 }
 
 
+/* Add a new account to the list. This first checks
+ * if the account is available by ID.
+ * If it is, it adds it, otherwise it indicates the
+ * account id is taken.
+ */
+template <typename T>
+void caseAddCustomer(SLinkedList<T>& list){
+
+    long int id;
+    string   name;
+    float    balance;
+    SNode<BasicAccount>* ptr;
+
+    cout << "\nTo create a new account, provide the following:\n";
+    cout << "Account id\t: ";
+    cin >> id;
+    cout << "Account name\t: ";
+    cin >> name;
+    cout << "Account balance\t: ";
+    cin >>balance;
+    BasicAccount ba(id, name, balance);
+    ptr = addNewAccount(list, ba);
+    if(ptr == NULL){
+        cout << "\nFailed to add account. ID already exists.\n";
+    }
+    else{
+        cout << "\nAccount successfully added.\n";
+        cout << ptr->data;
+    }
+}
+
+/* Delete an existing customer:
+ * This block allows the user to first search
+ * through the list of accounts using name or ID,
+ * then displays the account information before
+ * requesting confirmation if this is the account
+ * to delete. If it is, it is deleted. Otherwise,
+ * no action is taken.
+ */
+template <typename T>
+void caseDeleteCustomer(SLinkedList<T>& list){
+
+    string confirm;
+    string nameOrId;
+    string searchStr;
+    SNode<BasicAccount>* ptr;
+
+    cout << "\n\nWould you like to delete by id or name? ";
+    cin >> nameOrId;
+
+    if(nameOrId == "id"){
+        cout << "\nEnter id: ";
+        cin >> searchStr;
+        ptr = searchById(list, std::stol(searchStr));
+    }
+    else if(nameOrId == "name"){
+        cout << "\nEnter name: ";
+        cin >> searchStr;
+        ptr = searchByName(list, searchStr);
+    }
+    else{
+        cout << "\nInvalid command\n";
+        return;
+    }
+    if(ptr == NULL){
+        cout << "\nCould not locate account.\n";
+        return;
+    }
+    cout << "Found account. Details below:\n\n";
+    cout << ptr->data;
+    cout << "\nAre you sure you want to delete the account? ";
+    cin >> confirm;
+    if(confirm[0] == 'y' || confirm[0] == 'Y'){
+        list.erase(ptr);
+        cout << "\nAccount deleted.\n\n";
+    }
+    else cout << "\nNo action taken.\n\n";
+}
+
+/* Find an existing account by name or ID.
+ * This choice allows the user to select
+ * the desired method of searching, then prompts
+ * user to enter a name or id to searh by.
+ * If a valid account is found, it also prompts user
+ * if he/she would like to deposit or withdraw money.
+ */
+template <typename T>
+SNode<T>* caseFindCustomer(SLinkedList<T>& list){
+
+    string nameOrId;
+    string searchStr;
+    SNode<BasicAccount>* ptr;
+
+    cout << "\n\nWould you like to search by id or name? ";
+    cin >> nameOrId;
+
+    if(nameOrId == "id"){
+        cout << "\nEnter id: ";
+        cin >> searchStr;
+        ptr = searchById(list, std::stol(searchStr));
+    }
+    else if(nameOrId == "name"){
+        cout << "\nEnter name: ";
+        cin >> searchStr;
+        ptr = searchByName(list, searchStr);
+    }
+    else{
+        cout << "\nInvalid command\n";
+        return NULL;
+    }
+    if(ptr == NULL){
+        cout << "\nCould not locate account.\n";
+        return NULL;
+    }
+    cout << "Found account. Details below:\n\n";
+    cout << ptr->data;
+    return ptr;
+}
+
+
+/* AddNewAccount:
+ * This functions adds an account to the list
+ * sorted by ID number. It searches for an id that
+ * is greater than it, and inserts a new node before it.
+ * If there are none or if list is empty, it appends the node.
+ */
 template <typename T>
 SNode<T>* addNewAccount(SLinkedList<T>& list, BasicAccount& ba){
     SNode<T>* ptr = list.getHead();
@@ -207,9 +232,8 @@ SNode<T>* addNewAccount(SLinkedList<T>& list, BasicAccount& ba){
         ptr = list.append(ba);
         return ptr;
     }
-    while(ptr->data.getAccountNum() < ba.getAccountNum()){
+    while(ptr && ptr->data.getAccountNum() < ba.getAccountNum()){
         ptr = list.advance(ptr, 1);
-        if(!ptr) break;
     }
     if(ptr && ptr->data.getAccountNum() == ba.getAccountNum()) return NULL;
     if(ptr == NULL){
@@ -220,26 +244,38 @@ SNode<T>* addNewAccount(SLinkedList<T>& list, BasicAccount& ba){
     return ptr;
 }
 
+/* SearchById && SearchByName:
+ * These functions operate very similarly.
+ * They first check if the head is null.
+ * Otherwise, they iterate through each node comparing
+ * the given string or id to the node name or id.
+ * If there is a match, a ptr to the node is returned.
+ * Otherwise, NULL is returned.
+ */
 template <typename T>
 SNode<T>* searchById(SLinkedList<T>& list, long int id){
     SNode<T>* ptr = list.getHead();
-    if(ptr == NULL) return NULL;
-    while(ptr->data.getAccountNum() != id){
+    while(ptr && ptr->data.getAccountNum() != id){
         ptr = list.advance(ptr, 1);
-        if(!ptr) return NULL;
+        //if(!ptr) return NULL;
     }
     return ptr;
 }
 template <typename T>
 SNode<T>* searchByName(SLinkedList<T>& list, string name){
     SNode<T>* ptr = list.getHead();
-    if(ptr == NULL) return NULL;
-    while(ptr->data.getName() != name){
+    //if(ptr == NULL) return NULL;
+    while(ptr && ptr->data.getName() != name){
         ptr = list.advance(ptr, 1);
-        if(!ptr) return NULL;
     }
     return ptr;
 }
+
+
+/* depositOrWithdraw:
+ * Prompts user to withdraw or deposit money into an
+ * account given via pointer to node.
+ */
 template <typename T>
 void depositOrWithdraw(SNode<T>* ptr){
     int choice;
